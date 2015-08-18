f3840c0538d45bbccd6e8ae0552d56e879a93143
spanacom <marc@spanacom.com>
2015-08-18 11:44:49 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,10 +1,17 @@
-<?
-include("api.php");
+<?php
+require_once "api.php";
+try {
+$spanacom = new Spanacom("YOUR_API_KEY");
+$params= array("to"=> "34638125683",
+"from"=> "PUBLI",
+"text"=> "Hola esto es un mensaje de prueba");
 
-$spanacom = new Spanacom('MIAPIKEY');
-
-echo 'respuesta:<br><br>';
-$response=$spanacom->smsmt_send(Array('to'=>'34638638638','from'=>'MyBusiness','text'=>'Hello world'));
-print_r($response); // Testone
-
+$response=$spanacom->smsmt_send($params);
+if(!empty($response)){
+echo '$response';
+print_r($response);
+}
+} catch(Exception $e) {
+echo "error:".$e; 
+}
 ?>
