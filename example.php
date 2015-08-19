4f0338a801491125ba8a3a4c199a1395480afda5
spanacom <marc@spanacom.com>
2015-08-19 23:00:22 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,14 +1,16 @@
 <?php
 require_once "api.php";
-try {
 $spanacom = new Spanacom("YOUR_API_KEY");
+try {
 $params= array("to"=> "34638638638",
 "from"=> "FROMSPA",
 "text"=> "Hello world to msg");
 
 $response=$spanacom->smsmt_send($params);
+ echo "<br/><br/>SpanacomAPIOK:<br/>";
 print_r($response);
 } catch(Exception $e) {
-echo "SpanacomAPIerror:".$e; 
+echo "<br/><br/>SpanacomAPIerror:<br/>"; 
+print_r($e);
 }
 ?>
